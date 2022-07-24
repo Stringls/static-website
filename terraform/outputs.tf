442c6ce0239301c788d246e5667d28c4b124992c
vagrant <vagrant@localhost.localdomain>
2022-07-24 14:46:06 +0300

    rename website endpoint output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
 output "bucket_regional_domain_name" {
-  value       = module.bucket.bucket_regional_domain_name
+  value       = module.bucket.bucket_website_endpoint
   description = "S3 Bucket REGIONAL domain name"
 }
 
 output "cloudfront_domain_name" {
   value       = module.cloudfront.cloudfront_domain_name
   description = "Cloudfront domain name"
+}
+
+output "bucket_name" {
+  value = module.bucket.bucket_name
+  description = "S3 Bucket name"
 }
