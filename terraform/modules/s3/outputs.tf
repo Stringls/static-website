7d096bdc3dd82362a5f13e5de5c4b6df4096e62c
vagrant <vagrant@localhost.localdomain>
2022-07-24 14:46:12 +0300

    add bucket name output

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -2,3 +2,8 @@ output "bucket_website_endpoint" {
   value       = aws_s3_bucket.main.website_endpoint
   description = "Output s3 bucket website endpoint"
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.main.bucket
+  description = "S3 Bucket name"
+}
