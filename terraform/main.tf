5ce6af9c168430cff9dfae2d69330c30c814be86
vagrant <vagrant@localhost.localdomain>
2022-07-30 13:46:31 +0300

    change the ditribution settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,9 +59,10 @@ module "cloudfront" {
   repo_url = local.repo_url
 
   bucket_website_endpoint        = module.bucket.bucket_website_endpoint
-  main_origin_id                 = random_string.main.result
+  main_origin_id                 = module.bucket.bucket_name
   custom_origin_protocol_policy  = "https-only"
   enable_main_cloudfront         = true
+  is_ipv6_enabled                = true
   default_query_string           = false
   default_headers                = []
   default_cookies                = "none"
@@ -69,10 +70,45 @@ module "cloudfront" {
   main_max_ttl                   = 31536000
   main_default_ttl               = 86400
   default_cache_compress         = true
-  default_viewer_protocol_policy = "allow-all"
+  default_viewer_protocol_policy = "redirect-to-https"
   cloudfront_default_certificate = true
 
   depends_on = [
     module.bucket
   ]
 }
+
+# resource "aws_cloudfront_distribution" "distribution" {
+#   origin {
+#     domain_name = "unknown"
+#     origin_id = "unknown"
+#   }
+
+#   enabled = true
+
+#   default_cache_behavior {
+#     allowed_methods = []
+#     cached_methods = []
+#     target_origin_id = "unknown"
+
+#     forwarded_values {
+#       query_string = false
+
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+
+#     viewer_protocol_policy = "unknown"
+#   }
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "unknown"
+#     }
+#   }
+
+#   viewer_certificate {
+#     cloudfront_default_certificate = "unknown"
+#   }
+# }
